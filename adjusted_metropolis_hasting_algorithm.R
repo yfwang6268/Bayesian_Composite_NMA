d1c910fe06de1df3cf57ff8a948d10bdce5ab4df
Ethan <xawanyifei@hotmail.com>
2023-05-02 20:59:08 -0500

    update MCMC algorithm
    each outcome consider separately

--- adjusted_metropolis_hasting_algorithm.R
+++ adjusted_metropolis_hasting_algorithm.R
@@ -1,9 +1,6 @@
-sampling_mu_and_tau <- function(dataout, prev_tau, narm = 3){
-  proposed_mu = NULL
-  proposed_tau = NULL
-
-
-  for(k in 1:2){
+sampling_mu_and_tau <- function(dataout, k, narm = 3){
+  
+  
     tau_result = numeric(3)
     mu_result =numeric(3)
     for(t1 in 1:(narm-1)){
@@ -14,7 +11,7 @@ sampling_mu_and_tau <- function(dataout, prev_tau, narm = 3){
           within_study_variance = temp_data$sd1^2
         } else {
           observed_effects = temp_data$outcome2
-          within_study_variancence = temp_data$sd2^2
+          within_study_variance = temp_data$sd2^2
         }
         
         if(t1 == 1){
@@ -27,14 +24,11 @@ sampling_mu_and_tau <- function(dataout, prev_tau, narm = 3){
           tau_index = 2 # CA
         }
  
-        between_study_variance = prev_tau[(k-1)*3 + tau_index]
-
-        n = length(within_study_variance )
+        
+        n = length(within_study_variance)
 
         if(tau_result[tau_index] == 0){
-          tau_result[tau_index] = sampling_from_proposed_distribution(between_study_variance, 
-                                                                      within_study_variance, 
-                                                                      observed_effects)
+          tau_result[tau_index] = sampling_from_proposed_distribution(within_study_variance, observed_effects)
           temp_variance = 1/sum(1/(within_study_variance + tau_result[tau_index]))         
           mu_result[tau_index] = rnorm(1, mean(observed_effects), sqrt(temp_variance))
           } 
@@ -42,11 +36,9 @@ sampling_mu_and_tau <- function(dataout, prev_tau, narm = 3){
       
     }
 
-    proposed_mu = c(proposed_mu, mu_result)
-    proposed_tau = c(proposed_tau, tau_result)
-    
-  }
-  return(list(proposed_mu, proposed_tau))
+     
+  
+  return(list(mu_result, tau_result))
 }
 
 
@@ -54,34 +46,83 @@ adjusted_metropolis_hasting_algorithm <- function(dataout, chain_length,
                                                   burn_in_rate, narm = 3){
   
   
-  simulation_mu = matrix(nrow = chain_length,ncol = 6)
+  simulation_mu = matrix(nrow = chain_length+1,ncol = 6)
   simulation_tau = simulation_mu
-  
-  for(t in 1:chain_length){
-    if(t == 1){
-      previous_mu = c(0.4,0.8,-0.4,0.1,-0.5,0.6)
-      previous_tau = c(0.2,0.3,0.451,0.3,0.1,0.365)
-      simulation_mu[t,] = previous_mu
-      simulation_tau[t,] = previous_tau
+  for(k in 1:2){
+    for(t in 1: (chain_length + 1)){
+      if (t == 1){
+        if(k == 1){
+            previous_mu = c(0.4,0.8,-0.4)
+            previous_tau = c(0.2,0.3,0.451)
+          } else {
+            previous_mu = c(0.1,-0.5,0.6)
+            previous_tau = c(0.3,0.1,0.365)
+          }
+        simulation_mu[t,(k-1)*3+1:3] = previous_mu  
+        simulation_tau[t,(k-1)*3+1:3] = previous_tau
+      } else {
 
-    } else {
-      
-      # proposed new parameter value
-      
-      proposed_result = sampling_mu_and_tau(dataout, previous_tau)
-      
-      simulation_mu[t,] = proposed_result[[1]]
-      simulation_tau[t,] = proposed_result[[2]]
+        proposed_result = sampling_mu_and_tau(dataout, k)
+        proposed_mu = proposed_result[[1]]
+        proposed_tau = proposed_result[[2]]
 
-      previous_mu = proposed_result[[1]]
-      previous_tau = proposed_result[[2]]
+        numerator = product_propose_distribution(dataout, proposed_tau, proposed_mu, k)
+        denominator = product_propose_distribution(dataout, previous_tau, previous_mu, k)
+
+        #print(c(proposed_tau, proposed_mu))
+        a = numerator/denominator
+        if(runif(1) <= min(a,1)){
+          simulation_mu[t,(k-1)*3+1:3] = proposed_mu
+          simulation_tau[t,(k-1)*3+1:3] = proposed_tau
+          previous_mu = proposed_mu
+          previous_tau = proposed_tau
+        } else {
+          simulation_mu[t,(k-1)*3+1:3] = previous_mu
+          simulation_tau[t,(k-1)*3+1:3] = previous_tau
+        }
+
+
+      }
+
+
+
+
+
     }
+
+
+
+
   }
+  
+# for(t in 1:chain_length){
+#   if(t == 1){
+#     previous_mu = c(0.4,0.8,-0.4,0.1,-0.5,0.6)
+#     previous_tau = c(0.2,0.3,0.451,0.3,0.1,0.365)
+#     simulation_mu[t,] = previous_mu
+#     simulation_tau[t,] = previous_tau
+#     
+#   } else {
+#     
+#     # proposed new parameter value
+#     
+#     proposed_result = sampling_mu_and_tau(dataout, previous_tau)
+#     proposed_mu = proposed_result[[1]]
+#     
+#     
+#     
+#     simulation_mu[t,] = proposed_result[[1]]
+#     simulation_tau[t,] = proposed_result[[2]]
+#     
+#     previous_mu = proposed_result[[1]]
+#     previous_tau = proposed_result[[2]]
+#   }
+# }
   
   store_row = floor(chain_length * burn_in_rate) + 1
   
-  simulation_mu = simulation_mu[store_row:chain_length,]
-  simulation_tau = simulation_tau[store_row:chain_length,]
+  simulation_mu = simulation_mu[store_row:(chain_length+1),]
+  simulation_tau = simulation_tau[store_row:(chain_length+1),]
   result = list(simulation_mu, simulation_tau)
   return(result)
   
--- sampling_from_proposed_distribution.R
+++ sampling_from_proposed_distribution.R
@@ -11,39 +11,44 @@ pdf_proposed_distribution <- function(between_study_variance, within_study_varia
 
 
 
-sampling_from_proposed_distribution <- function(between_study_variance, within_study_variance, observed_effects){
+sampling_from_proposed_distribution <- function(within_study_variance, observed_effects, simu_length=500){
   n = length(observed_effects)
-  previous_value = between_study_variance
-
-  for(t in 1:1000){
+  simulated_value = numeric(simu_length + 1)
+  count = 0
+  for(t in 1:(simu_length+ 1)){
       #proposed_value = rinvgamma(1, 1)
       phi = runif(1)
       proposed_value = exp(phi)
       if(t > 1){
-        #numerator = pdf_proposed_distribution(proposed_value, within_study_variance, observed_effects)*dinvgamma(previous_value, 1)
         numerator = pdf_proposed_distribution(proposed_value, within_study_variance, observed_effects)
-        #denominator = pdf_proposed_distribution(previous_value , within_study_variance, observed_effects)*dinvgamma(proposed_value, 1)
         denominator = pdf_proposed_distribution(previous_value , within_study_variance, observed_effects)
         a_proposed_distribution = numerator/denominator
         if(runif(1) <= min(a_proposed_distribution, 1)){
+          simulated_value[t] = proposed_value
           previous_value = proposed_value
+          } else {
+          simulated_value[t] = previous_value
           }
       } else {
+        simulated_value[t] = proposed_value
         previous_value = proposed_value
       }
-  }         
-  return(previous_value)
+  
+  } 
+
+  return(simulated_value[simu_length + 1])
 }
 
-product_propose_distribution <- function(dataout, tau, mu, narm = 3){
-  product_probability = 1
-  for(k in 1:2){
-    temp_tau = tau[(k-1)*3+1:narm]
-    temp_mu = mu[(k-1)*3+1:narm]
+product_propose_distribution <- function(dataout, tau, mu, k, narm = 3){
+    product_probability = 1
 
+    temp_tau = tau
+    temp_mu = mu
+    
     for(t1 in 1:(narm-1)){
       for(t2 in (t1+1):narm){
         temp_data = subset(dataout, dataout$t1 == t1 & dataout$t2 == t2)
+
         if(k == 1){
           observed_effects  = temp_data$outcome1
           within_study_variance = temp_data$sd1^2
@@ -51,6 +56,7 @@ product_propose_distribution <- function(dataout, tau, mu, narm = 3){
           observed_effects = temp_data$outcome2
           within_study_variance = temp_data$sd2^2
         }
+
         if(t1 == 1){
           if(t2 == 3){
             comparision_index = 1 # BA
@@ -62,12 +68,15 @@ product_propose_distribution <- function(dataout, tau, mu, narm = 3){
         }
         between_study_variance = temp_tau[comparision_index]
         effect_size = temp_mu[comparision_index]
+        temp_mu_variance = 1/sum(1/(within_study_variance + between_study_variance))
+
+
         product_probability = product_probability  * 
                               pdf_proposed_distribution(between_study_variance, within_study_variance, observed_effects) *
-                              dnorm(effect_size, mean(observed_effects), sqrt(between_study_variance))
+                              dnorm(effect_size, mean(observed_effects), sqrt(temp_mu_variance))
       }
     }
-  }
+  
   return(product_probability)
 }
 
